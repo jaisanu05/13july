6acb70edaf5bf459dc4a206bef1dc4d53956480e
EC2 Default User <ec2-user@ip-172-31-5-128.ap-south-1.compute.internal>
2024-07-13 04:37:56 +0000

    Changes made in master

--- script.sh
+++ script.sh
@@ -9,13 +9,18 @@ if [ "$name" = "admin" -a "$pass" = "admin123" ]
 
 then
 	echo "Authentication is successful"
+
+elif [ "$name" = "Developer" -a "$pass" = "JavaPython@123" ]
+then
+
+	 echo "Authentication is successful"
 else 
 	echo "Authentication unsuccessful"
 fi
 
 read -p "Enter a charachter " c
 
-if [ "$c" = "a" -o "$c" = "e" -o "$c" = "i" -o "$c" = "o" -o "$c" = "u" ]
+if [ "$c" = "a" -o "$c" = "e" -o "$c" = "i" -o "$c" = "o" -o "$c" = "u" -o "$c" = "A" -o "$c" = "E" -o "$c" = "I" -o "$c" = "O" -o "$c" = "U" ]
 then
 	echo "Given Charachter is a Vowel"
 else
