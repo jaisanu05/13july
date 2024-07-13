0cba06f2ee28eb6adb11ca7f1d32cb0a44b566db
EC2 Default User <ec2-user@ip-172-31-5-128.ap-south-1.compute.internal>
2024-07-13 04:16:05 +0000

    Adding in master branch

---
+++ script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+read -p "Enter Username " name
+#echo "Enter Your password"
+#read -s password pass
+read -p "Enter Password " pass
+
+if [ "$name" = "admin" -a "$pass" = "admin123" ]
+
+then
+	echo "Authentication is successful"
+else 
+	echo "Authentication unsuccessful"
+fi
+
+read -p "Enter a charachter " c
+
+if [ "$c" = "a" -o "$c" = "e" -o "$c" = "i" -o "$c" = "o" -o "$c" = "u" ]
+then
+	echo "Given Charachter is a Vowel"
+else
+	echo "Given Charachter is not a Vowel"
+fi
+
+read -p "Enter Your Age " d
+
+if [ $d -lt 13 ]
+then
+	echo "Child"
+
+elif [ $d -ge 13 -a $d -le 19 ]
+then
+	echo "teenager"
+
+elif [ $d -ge 20 ]
+then
+	echo "Adult"
+fi
